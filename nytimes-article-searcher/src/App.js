0d34ee5fe421a660177bdc5c33eb22425bfd0733
Matthew Berglund <bergitastrophy@gmail.com>
2022-02-24 16:59:03 +0100

    create react app, add header

---
+++ nytimes-article-searcher/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>NYT Article Search</h1>
+      </header>
+    );
+  }
+}
+
+export default App;
