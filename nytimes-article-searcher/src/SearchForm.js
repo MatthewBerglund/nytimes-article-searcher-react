f2153210cfe6fa64a9a42275eb1c82da1f0bc674
Matthew Berglund <bergitastrophy@gmail.com>
2022-03-04 08:30:30 +0100

    Remove form functionality

--- nytimes-article-searcher/src/SearchForm.js
+++ nytimes-article-searcher/src/SearchForm.js
@@ -3,22 +3,13 @@ import NewsdeskFieldset from './NewsdeskFieldset';
 import MaterialTypeFieldset from './MaterialTypeFieldset';
 
 class SearchForm extends React.Component {
-  filterMenu = React.createRef();
-  filtersButton = React.createRef();
-  
-  toggleFilterMenuVisibility = () => {
-    if (this.filterMenu.style.display === '') {
-      this.filterMenu.style.display = 'grid';
-      this.filtersButton.textContent = 'Hide filters';
-    } else {
-      this.filterMenu.style.display = '';
-      this.filtersButton.textContent = 'Show filters';
-    }
-  }
+  state = {
+    isMenuOpen: false
+  };
   
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSearchSubmit}>
         <div id="search-controls-container">
           <div>
             <input id="query-input" type="search" placeholder="Enter a search term" />
@@ -33,7 +24,7 @@ class SearchForm extends React.Component {
           </div>
           <button id="submit">Search</button>
         </div>
-        <div id="filters-container" ref={this.filterMenu}>
+        <div id="filters-container" className={this.state.isMenuOpen ? 'open' : ''}>
           <NewsdeskFieldset />
           <MaterialTypeFieldset />
           <div>
@@ -41,11 +32,17 @@ class SearchForm extends React.Component {
             <input type="search" id="location-search" />
           </div>
         </div>
-        <button id="filters-button" ref={this.filtersButton} onClick={event => {
-          event.preventDefault();
-          this.toggleFilterMenuVisibility();
-        }}>
-          Show filters
+        <button 
+          id="filters-button"  
+          onClick={event => {
+            event.preventDefault();
+            const isMenuOpen = this.state.isMenuOpen;
+            this.setState({
+              isMenuOpen: !isMenuOpen
+            });
+          }
+        }>
+          {this.state.isMenuOpen ? 'Hide filters': 'Show filters'}
         </button>
       </form>
     );
