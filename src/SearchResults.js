d2572997791d4c467c360daeaea729cdf19f711e
Matthew Berglund <bergitastrophy@gmail.com>
2022-03-19 15:45:23 +0100

    Destructure props used locally by component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,9 +3,11 @@ import Article from './Article';
 
 class SearchResults extends React.Component {
   render() {
+    const {articles, isFetching} = this.props;
+
     return (
-      <ul id="articles-container" className={this.props.isFetching ? 'loading' : ''}>
-        {this.props.articles.map(article => 
+      <ul id="articles-container" className={isFetching ? 'loading' : ''}>
+        {articles.map(article => 
           <Article 
             key={article._id}
             article={article}
