606dcfb139f5be1fc8492b20d7e779b65bf35376
Matthew Berglund <bergitastrophy@gmail.com>
2022-03-19 14:56:19 +0100

    Change bracket notation to dot notation

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends React.Component {
       <ul id="articles-container" className={this.props.isFetching ? 'loading' : ''}>
         {this.props.articles.map(article => 
           <Article 
-            key={article['_id']}
+            key={article._id}
             article={article}
             urlSearchParams={this.props.urlSearchParams}
             setUrlSearchParams={this.props.setUrlSearchParams}
