6113a1dbced4f7e858b9572877aee6f712168a49
Matthew <bergitastrophy@gmail.com>
2022-07-24 08:49:46 +0200

    Move article styles to css module

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,3 +1,5 @@
+import styles from './Article.module.css';
+
 import Keyword from '../components/Keyword';
 
 const Article = ({ article }) => {
@@ -6,30 +8,28 @@ const Article = ({ article }) => {
   const thumbnail = multimedia.find(image => image.subtype === 'blog225');
 
   return (
-    <li>
-      <article>
-        <a href={web_url} target="_blank" className="headline-link">
-          <h2>{headline.main}</h2>
-        </a>
-        {abstract ? (
-          <p className="article-abstract">{abstract}</p>
-        ) : null}
-        {thumbnail ? (
-          <img
-            src={`http://www.nytimes.com/${thumbnail.url}`}
-            className="article-img"
-            alt="article thumbnail"
-          />
-        ) : null}
-        {keywords.length > 0 ? (
-          <ul className="keywords">
-            {keywords.map((keyword, index) => (
-              <Keyword key={index} keyword={keyword} />
-            ))}
-          </ul>
-        ) : null}
-      </article>
-    </li>
+    <article>
+      <a href={web_url} target="_blank" className={styles.headline}>
+        <h2>{headline.main}</h2>
+      </a>
+      {abstract ? (
+        <p className={styles.abstract}>{abstract}</p>
+      ) : null}
+      {thumbnail ? (
+        <img
+          src={`http://www.nytimes.com/${thumbnail.url}`}
+          className={styles.thumbnail}
+          alt="article thumbnail"
+        />
+      ) : null}
+      {keywords.length > 0 ? (
+        <ul className={styles.keywords}>
+          {keywords.map((keyword, index) => (
+            <Keyword key={index} keyword={keyword} />
+          ))}
+        </ul>
+      ) : null}
+    </article>
   );
 }
 
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,7 +4,9 @@ const SearchResults = ({ articles, isLoading }) => {
   return (
     <ul id="articles-container" className={isLoading ? 'loading' : ''}>
       {articles.map(article =>
-        <Article key={article._id} article={article} />
+        <li key={article._id}>
+          <Article article={article} />
+        </li>
       )}
     </ul>
   );
